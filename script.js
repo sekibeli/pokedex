0d6d86eabf60ac699569ea51f02918b83d8a26f0
Julia Georgiew <java2011@me.com>
2023-03-08 17:48:45 +0100

    first 30

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-let currentPokemon;
+let pokemons = [];
 
-async function loadPokemon(){
-    let url = `https://pokeapi.co/api/v2/pokemon/pikachu`;
+let pokemonsOnScreen;
+let count = 1;
+
+async function load30Pokemon(){
+    
+    for (i = count; i <= count + 30; i++){
+    let url = `https://pokeapi.co/api/v2/pokemon/${i}/`;
     let response = await fetch(url);
-    currentPokemon = await response.json();
-    console.log('Server antortet: ' , currentPokemon);
-   renderPokemonInfo();
+    let pokemon = await response.json();
+    pokemons.push(pokemon);
+   
+  
+    }
+    console.log('Server antwortet: ' , pokemons);
+    renderPokemonInfo();
 }
 
 function renderPokemonInfo(){
-    document.getElementById('pokemonName').innerHTML = currentPokemon.name;
-    document.getElementById('pic').src = `${currentPokemon.sprites.other.dream_world.front_default}`;
+    for (j=0; j<pokemons.length; j++)
+
+    document.getElementById(`main`).innerHTML += `
+    <div class="card"><div class="pokedexCard" id="pokedex${j}">
+        <h2 id="pokemonName${j}">${pokemons[j].name}</h2>
+        <img class="pokeImg" src=${pokemons[j].sprites.other.dream_world.front_default}>
+    </div>  <div class="info-container"></div></div>`;
+    
 }
