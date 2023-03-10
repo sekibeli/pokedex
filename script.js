2c0ac0cc7cd38565ca96722c8f332ebfb71c28f8
Julia Georgiew <java2011@me.com>
2023-03-10 20:46:14 +0100

    moves

--- script.js
+++ script.js
@@ -43,13 +43,13 @@ function renderPokemonInfo() {
              <nav class="line">
                  <ul class="menu">
                      <li>About</li>
-                    <li id="baseStats${j}" onclick="">Base Stats</li>
-                     <li>Moves</li>
+                    <li id="stats${j}" onclick="show('statsContainer${j}', 'movesContainer${j}')">Base Stats</li>
+                     <li id="moves${j}" onclick="show('movesContainer${j}', 'statsContainer${j}')">Moves</li>
              </nav>
             
         <div id="about" class="info"></div>
-        <div id="stats${j}" class="statsContainer"></div>
-        <div id="moves" class="info"></div>
+        <div id="statsContainer${j}" class="statsContainer"></div>
+        <div id="movesContainer${j}" class="movesContainer d-none"></div>
        
         </div>
         </div>`;
@@ -57,6 +57,7 @@ function renderPokemonInfo() {
         renderTypes(j);
         renderStats(j);
         getMax(j);
+        renderMoves(j);
     }
 }
 
@@ -68,7 +69,7 @@ function renderTypes(j) {
 
 function renderStats(j){
     for (let i = 0; i < pokemons[j].stats.length; i++) {
-        document.getElementById(`stats${j}`).innerHTML += ` <div class="infoStats"><span class="statName">${pokemons[j].stats[i].stat.name}</span>
+        document.getElementById(`statsContainer${j}`).innerHTML += ` <div class="infoStats"><span class="statName">${pokemons[j].stats[i].stat.name}</span>
         <span class="statAmount">${pokemons[j].stats[i].base_stat}</span>  <div class="progress" role="progressbar" aria-label="Example 20px high" 
         aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="height: 5px">
         <div class="progress-bar ${pokemons[j].types[0].type.name}" style="width: ${getCorrectBar(i,j)}%"></div>
@@ -76,23 +77,32 @@ function renderStats(j){
     }
 }
 
-function show(param){
-    document.getElementById(`${param0}`).classList.remove('d-none');
+
+function show(param1, param2){
+    document.getElementById(`${param1}`).classList.remove('d-none');
+    document.getElementById(`${param2}`).classList.add('d-none');
 }
+
 
 function getMax(j){
 let numbers = [];
-for (i=0; i< pokemons[j].stats.length; i++){
+for (let i=0; i< pokemons[j].stats.length; i++){
 numbers.push(pokemons[j].stats[i].base_stat);
 numbers.sort(function(a,b){return a-b});
 }
-
-  
 return numbers;
 }
+
 
 function getCorrectBar(i, j){
     let numbers = getMax(j);
       return (100 / numbers[5] * pokemons[j].stats[i].base_stat);
 }
-// ${pokemons[j].stats[i].base_stat}
+
+
+function renderMoves(j){
+    for (let i=0; i< pokemons[j].moves.length; i++){
+        document.getElementById(`movesContainer${j}`).innerHTML += `<span>${pokemons[j].moves[i].move.name} </span>`;
+        console.log(pokemons[j].moves[i].move.name);
+    }
+}
